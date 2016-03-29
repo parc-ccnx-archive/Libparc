6cfe5f6d42accdd3f8a975466374095d2fa13315
Glenn Scott <Glenn.Scott@parc.com>
2016-03-29 14:05:00 -0700

    Initial workspace

---
+++ examples/How To Extend a PARCObject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  How To Extend a PARCObject
+//
+//  Created by gscott on 3/29/16.
+//  Copyright © 2016 CSL. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
